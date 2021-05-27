c4a9047b949e47b462ced2359ee3658b27eb7ffc
shahin68 <shahin.montazeri@gmail.com>
2021-05-27 10:31:33 +0430

    extension to extract clean error message

---
+++ app/src/main/java/com/shahin/meistersearch/general/extensions/ErrorExtensions.kt
@@ -0,0 +1,20 @@
+package com.shahin.meistersearch.general.extensions
+
+import android.content.Context
+import com.shahin.meistersearch.data.remote.models.response.error.ErrorResponse
+
+/**
+ * extension to extract clean message from error response model
+ * it returns a user friendly message
+ */
+fun ErrorResponse?.extractCleanMessage(context: Context): String? {
+    val stringBuilder = StringBuilder()
+    this?.errors?.let {
+        it.take(it.size).forEach {
+            stringBuilder.append(it.status)
+            stringBuilder.append("\n")
+            stringBuilder.append(it.message)
+        }
+    }
+    return if (stringBuilder.toString().isEmpty()) null else  stringBuilder.toString()
+}
