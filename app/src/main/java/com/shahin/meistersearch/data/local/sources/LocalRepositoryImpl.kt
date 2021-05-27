6f10213995113fe078f3d316afd162f8027ed87a
shahin68 <shahin.montazeri@gmail.com>
2021-05-27 10:30:54 +0430

    build bearer token

--- app/src/main/java/com/shahin/meistersearch/data/local/sources/LocalRepositoryImpl.kt
+++ app/src/main/java/com/shahin/meistersearch/data/local/sources/LocalRepositoryImpl.kt
@@ -5,7 +5,7 @@ import com.shahin.meistersearch.BuildConfig
 class LocalRepositoryImpl: LocalRepository {
 
     override fun getToken(): String {
-        return BuildConfig.ACCESS_TOKEN
+        return "${BuildConfig.ACCESS_TOKEN_PREFIX} ${BuildConfig.ACCESS_TOKEN}"
     }
 
 }
